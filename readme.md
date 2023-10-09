8f923320b5e8eb328c66b614be5eb5cecc74025e
rickdev93 <henriquedev93@gmail.com>
2023-10-09 14:40:42 -0300

    First commit, final project

---
+++ readme.md
@@ -0,0 +1,13 @@
+### Project developed at PystackWeek 8.0
+
+Tecnologies used:
+
+- Python: main language.
+
+- Django: main framework.
+
+- HTML, CSS, Bootstrap: frontend.
+
+- Javascript: single script.
+
+The project consists of creating a website for registering patients, doctors and laboratory tests. All access using passwords.
---
+++ usuarios/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+# Create your views here.
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        primeiro_nome = request.POST.get('primeiro_nome').title()
+        ultimo_nome = request.POST.get('ultimo_nome').title()
+        username = request.POST.get('username').lower()
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        # user = authenticate(username = username, password = senha)
+
+        # if user:
+        #     messages.add_message(request, constants.ERROR, 'Este nome de usuário já existe.')
+        #     return redirect('/usuarios/cadastro')
+
+        if not senha == confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'As senhas não coincidem.')
+            return redirect('/usuarios/cadastro')
+
+        if len(senha) < 6:
+            messages.add_message(request, constants.ERROR, 'A senha precisa ter no mínimo 6 caracteres.')
+            return redirect('/usuarios/cadastro')
+
+        try:
+            user = User.objects.create_user(
+                first_name = primeiro_nome,
+                last_name = ultimo_nome,
+                username = username,
+                email = email,
+                password = senha,
+            )
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno, tente novamente.')
+            return redirect('/usuarios/cadastro')
+
+
+        messages.add_message(request, constants.SUCCESS, 'Usuário cadastrado com sucesso.')
+        return redirect('/usuarios/cadastro')
+
+def logar(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username').lower()
+        senha = request.POST.get('senha')
+        user = authenticate(username = username, password = senha)
+
+        if user:
+            login(request, user)
+            return redirect('/exames/solicitar_exames')
+        else:
+            messages.add_message(request, constants.ERROR, 'Usuario ou senha inválidos')
+            return redirect('/usuarios/login')
